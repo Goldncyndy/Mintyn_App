4962b216759ebbd4bfcee985fcfc1b8e3dda822c
Goldncyndy <ezehcynthia84@gmail.com>
2022-03-12 00:52:00 +0100

    Implement Home Screen

---
+++ CodeChallenge/CodeChallenge/Extension+HomeViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ExtensionHomeVC.swift
+//  CodeChallenge
+//
+//  Created by Decagon on 12/03/2022.
+//
+
+import UIKit
+
+extension HomeViewController {
+  
+  // MARK: - FUNCTION TO SETUP VIEWS ON SCREEN
+  func setupViewsConstraint() {
+    view.addSubview(instructionLabel)
+    view.addSubview(userImageView)
+    view.addSubview(cardView)
+    cardView.addSubview(walletLabel)
+    cardView.addSubview(balanceLabel)
+    view.addSubview(optionLabel)
+    view.addSubview(optionImageView1)
+    view.addSubview(optionImageView2)
+    view.addSubview(optionImageView3)
+    view.addSubview(optionImageView4)
+    view.addSubview(optionImageView5)
+    view.addSubview(optionImageView6)
+    view.addSubview(containerView1)
+    view.addSubview(containerView2)
+    view.addSubview(containerView3)
+    view.addSubview(containerView4)
+    view.addSubview(containerView5)
+    view.addSubview(containerView6)
+    navigationController?.navigationBar.isHidden = true
+    
+    NSLayoutConstraint.activate([
+      instructionLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+      instructionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+      userImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+      userImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+      userImageView.widthAnchor.constraint(equalToConstant: 60),
+      userImageView.heightAnchor.constraint(equalToConstant: 65),
+      // MARK: - VIEW CONSTRAINTS
+      cardView.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 30),
+      cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+      cardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+      cardView.heightAnchor.constraint(equalToConstant: 170),
+      //MARK: - WALLET LABEL CONSTRAINTS
+      walletLabel.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 40),
+      walletLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 20),
+      //MARK: - BALANCE LABEL CONSTRAINTS
+      balanceLabel.topAnchor.constraint(equalTo: walletLabel.bottomAnchor, constant: 5),
+      balanceLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 20),
+      // MARK: - OPTION LABEL CONSTRAINTS
+      optionLabel.topAnchor.constraint(equalTo: cardView.bottomAnchor, constant: 30),
+      optionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+      // MARK: - CONTAINER VIEW 1
+      containerView1.topAnchor.constraint(equalTo: optionLabel.bottomAnchor, constant: 20),
+      containerView1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+      containerView1.widthAnchor.constraint(equalToConstant: 170),
+      containerView1.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - MEAL OPTION VIEW 1 CONTRAINTS
+      optionImageView1.topAnchor.constraint(equalTo: containerView1.topAnchor, constant: 5),
+      optionImageView1.leadingAnchor.constraint(equalTo: containerView1.leadingAnchor, constant: 2),
+      optionImageView1.trailingAnchor.constraint(equalTo: containerView1.trailingAnchor, constant: -2),
+      optionImageView1.widthAnchor.constraint(equalToConstant: 170),
+      optionImageView1.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - CONTAINER VIEW 2
+      containerView2.topAnchor.constraint(equalTo: optionLabel.bottomAnchor, constant: 20),
+      containerView2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+      containerView2.widthAnchor.constraint(equalToConstant: 170),
+      containerView2.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - MEAL OPTION VIEW 2 CONTRAINTS
+      optionImageView2.topAnchor.constraint(equalTo: containerView2.topAnchor, constant: 5),
+      optionImageView2.leadingAnchor.constraint(equalTo:containerView2.leadingAnchor, constant: 2),
+      optionImageView2.trailingAnchor.constraint(equalTo: containerView2.trailingAnchor, constant: -2),
+      optionImageView2.widthAnchor.constraint(equalToConstant: 170),
+      optionImageView2.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - CONTAINER VIEW 3
+      containerView3.topAnchor.constraint(equalTo: containerView2.bottomAnchor, constant: 20),
+      containerView3.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+      containerView3.widthAnchor.constraint(equalToConstant: 170),
+      containerView3.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - MEAL OPTION VIEW 3 CONTRAINTS
+      optionImageView3.topAnchor.constraint(equalTo: containerView3.topAnchor, constant: 5),
+      optionImageView3.leadingAnchor.constraint(equalTo: containerView3.leadingAnchor, constant: 2),
+      optionImageView3.trailingAnchor.constraint(equalTo: containerView3.trailingAnchor, constant: -2),
+      optionImageView3.widthAnchor.constraint(equalToConstant: 170),
+      optionImageView3.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - CONTAINER VIEW 4
+      containerView4.topAnchor.constraint(equalTo: containerView2.bottomAnchor, constant: 20),
+      containerView4.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+      containerView4.widthAnchor.constraint(equalToConstant: 170),
+      containerView4.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - MEAL OPTION VIEW 4 CONTRAINTS
+      optionImageView4.topAnchor.constraint(equalTo: containerView4.topAnchor, constant: 5),
+      optionImageView4.leadingAnchor.constraint(equalTo: containerView4.leadingAnchor, constant: 2),
+      optionImageView4.trailingAnchor.constraint(equalTo: containerView4.trailingAnchor, constant: -2),
+      optionImageView4.widthAnchor.constraint(equalToConstant: 170),
+      optionImageView4.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - CONTAINER VIEW 5
+      containerView5.topAnchor.constraint(equalTo: containerView3.bottomAnchor, constant: 20),
+      containerView5.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+      containerView5.widthAnchor.constraint(equalToConstant: 170),
+      containerView5.heightAnchor.constraint(equalToConstant: 100),
+       // MARK: - MEAL OPTION VIEW 5 CONTRAINTS
+      optionImageView5.topAnchor.constraint(equalTo: containerView5.topAnchor, constant: 5),
+      optionImageView5.leadingAnchor.constraint(equalTo: containerView3.leadingAnchor, constant: 2),
+      optionImageView5.trailingAnchor.constraint(equalTo: containerView2.trailingAnchor, constant: -2),
+      optionImageView5.widthAnchor.constraint(equalToConstant: 170),
+      optionImageView5.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - CONTAINER VIEW 6
+      containerView6.topAnchor.constraint(equalTo: containerView3.bottomAnchor, constant: 20),
+      containerView6.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+      containerView6.widthAnchor.constraint(equalToConstant: 170),
+      containerView6.heightAnchor.constraint(equalToConstant: 100),
+      // MARK: - MEAL OPTION VIEW 6 CONTRAINTS
+      optionImageView6.topAnchor.constraint(equalTo: containerView6.topAnchor, constant: 5),
+      optionImageView6.leadingAnchor.constraint(equalTo: containerView6.leadingAnchor, constant: 2),
+      optionImageView6.trailingAnchor.constraint(equalTo: containerView6.trailingAnchor, constant: -2),
+      optionImageView6.widthAnchor.constraint(equalToConstant: 170),
+      optionImageView6.heightAnchor.constraint(equalToConstant: 100)
+      ])
+  }
+  
+}
--- CodeChallenge/CodeChallenge/HomeViewController.swift
+++ CodeChallenge/CodeChallenge/HomeViewController.swift
@@ -1,1 +1,211 @@
+//
+//  HomeViewController.swift
+//  CodeChallenge
+//
+//  Created by Decagon on 11/03/2022.
+//
 
+import UIKit
+
+class HomeViewController: UIViewController {
+  
+  let instructionLabel: UILabel = {
+    let label = UILabel()
+    label.text = " Welcome \n home, Smith!"
+    label.numberOfLines = 0
+    label.font = UIFont(name: "Helvetica", size: 35)
+    label.textAlignment = .left
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  // MARK: - IMAGE VIEW
+  lazy var userImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "profile2")
+    imageView.contentMode = .scaleAspectFit
+    imageView.layer.cornerRadius = 30.0
+    imageView.clipsToBounds = true
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+  
+  // MARK: - CARD VIEW
+  let cardView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .orange
+    view.layer.cornerRadius = 15
+    view.isUserInteractionEnabled = true
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  let walletLabel: UILabel = {
+    let label = UILabel()
+    label.text = " WALLET BALANCE"
+    label.textColor = .white
+    label.numberOfLines = 0
+    label.font = UIFont(name: "Helvetica", size: 14)
+    label.textAlignment = .left
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  let balanceLabel: UILabel = {
+    let label = UILabel()
+    label.text = " N503,700.00"
+    label.textColor = .white
+    label.numberOfLines = 0
+    label.font = UIFont(name: "Helvetica", size: 40)
+    label.font = UIFont.systemFont(ofSize: 40, weight: .bold)
+    label.textAlignment = .left
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  let optionLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Make your choice"
+    label.numberOfLines = 2
+    label.font = UIFont(name: "Helvetica", size: 16)
+    label.textAlignment = .center
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
+  // MARK: - IMAGE CONTAINER VIEW
+ let containerView1: UIView = {
+  let container = UIView()
+   container.layer.cornerRadius = 15
+   container.layer.masksToBounds = true
+   container.isUserInteractionEnabled = true
+   container.translatesAutoresizingMaskIntoConstraints = false
+   return container
+  }()
+  
+  // MARK: - IMAGE LABEL
+  lazy var optionImageView1: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "fried-rice")
+    imageView.layer.cornerRadius = 15.0
+    imageView.clipsToBounds = true
+    imageView.frame = containerView1.bounds
+    imageView.contentMode = .scaleAspectFill
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
+  // MARK: - IMAGE CONTAINER VIEW
+  let containerView2: UIView = {
+  let container = UIView()
+   container.layer.cornerRadius = 15
+   container.layer.masksToBounds = true
+   container.isUserInteractionEnabled = true
+   container.translatesAutoresizingMaskIntoConstraints = false
+   return container
+  }()
+  
+  // MARK: - IMAGE LABEL
+  lazy var optionImageView2: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "egusi_soup")
+    imageView.layer.cornerRadius = 25.0
+    imageView.clipsToBounds = true
+    imageView.frame = containerView2.bounds
+    imageView.contentMode = .scaleAspectFill
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+  
+  // MARK: - IMAGE CONTAINER VIEW
+  let containerView3: UIView = {
+  let container = UIView()
+   container.layer.cornerRadius = 15
+   container.layer.masksToBounds = true
+   container.isUserInteractionEnabled = true
+   container.translatesAutoresizingMaskIntoConstraints = false
+   return container
+  }()
+  
+  // MARK: - IMAGE LABEL
+  lazy var optionImageView3: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "Coconut Jollof rice")
+    imageView.layer.cornerRadius = 25.0
+    imageView.clipsToBounds = true
+    imageView.frame = containerView3.bounds
+    imageView.contentMode = .scaleAspectFill
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
+  // MARK: - IMAGE CONTAINER VIEW
+  let containerView4: UIView = {
+  let container = UIView()
+   container.layer.cornerRadius = 15
+   container.layer.masksToBounds = true
+   container.isUserInteractionEnabled = true
+   container.translatesAutoresizingMaskIntoConstraints = false
+   return container
+  }()
+  
+  // MARK: - IMAGE LABEL
+  lazy var optionImageView4: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "okra2")
+    imageView.layer.cornerRadius = 25.0
+    imageView.clipsToBounds = true
+    imageView.frame = containerView4.bounds
+    imageView.contentMode = .scaleAspectFill
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
+  // MARK: - IMAGE CONTAINER VIEW
+  let containerView5: UIView = {
+  let container = UIView()
+   container.layer.cornerRadius = 15
+   container.layer.masksToBounds = true
+   container.isUserInteractionEnabled = true
+   container.translatesAutoresizingMaskIntoConstraints = false
+   return container
+  }()
+  
+  lazy var optionImageView5: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "okra2")
+    imageView.layer.cornerRadius = 25.0
+    imageView.clipsToBounds = true
+    imageView.frame = containerView5.bounds
+    imageView.contentMode = .scaleAspectFill
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
+  // MARK: - IMAGE CONTAINER VIEW
+  let containerView6: UIView = {
+  let container = UIView()
+   container.layer.cornerRadius = 15
+   container.layer.masksToBounds = true
+   container.isUserInteractionEnabled = true
+   container.translatesAutoresizingMaskIntoConstraints = false
+   return container
+  }()
+  // MARK: - IMAGE LABEL
+  lazy var optionImageView6: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "okro-soup")
+    imageView.layer.cornerRadius = 25.0
+    imageView.clipsToBounds = true
+    imageView.frame = containerView6.bounds
+    imageView.contentMode = .scaleAspectFill
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      setupViewsConstraint()
+      view.backgroundColor = .systemBackground
+    }
+}
